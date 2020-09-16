608aa72b2310f2a88299dd1e7f0efad4e1578e7a
Patryk Gajek <pgajek94@gmail.com>
2020-09-16 18:54:08 +0200

    checking username and email, setSuccessFor and setErrorFor functions

--- main.js
+++ main.js
@@ -4,10 +4,26 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkInputs();
-});
+function setErrorFor(input, message) {
+  const formControl = input.closest(".form-control");
+  const error = formControl.querySelector(".errorMessage");
+
+  error.innerText = message;
+
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.closest(".form-control");
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+
 function checkInputs() {
   const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
@@ -15,10 +31,21 @@ function checkInputs() {
   const password2Value = password2.value.trim();
 
   if (usernameValue === "") {
-    //show errror
-    //add error class
+    setErrorFor(username, "Username can not be blank");
   } else {
-    //add success class
+    setSuccessFor(username);
+  }
+  if (emailValue === "") {
+    setErrorFor(email, "E-mail can not be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "E-mail can not be blank");
+  } else {
+    setSuccessFor(email);
   }
 }
 //trim remover whitespace from both ends
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
