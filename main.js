967d3bed255da90280f620aebb91d6dfe04dfec3
Patryk Gajek <pgajek94@gmail.com>
2020-09-16 18:31:33 +0200

    declarations of variables, triming values

---
+++ main.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+function checkInputs() {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {
+    //show errror
+    //add error class
+  } else {
+    //add success class
+  }
+}
+//trim remover whitespace from both ends
